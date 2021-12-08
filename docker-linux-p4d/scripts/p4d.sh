77273c527895d79fc19ec8248c00e55871ba2c5a
root <root@heizung>
2021-12-08 16:42:31 +0100

    fix startup scripts

---
+++ docker-linux-p4d/scripts/p4d.sh
@@ -0,0 +1,7 @@
+cp /home/p4d/daemon.conf /etc/p4d/daemon.conf
+cp /home/p4d/ssmtp.conf /etc/ssmtp/ssmtp.conf
+cp /home/p4d/revaliases /etc/ssmtp/revaliases
+
+echo "Start p4d"
+/usr/bin/p4d -n -t
+
--- docker-linux-p4d/scripts/start.sh
+++ docker-linux-p4d/scripts/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+touch /home/p4d/daemon.conf
+touch /home/p4d/ssmtp.conf
+touch /home/p4d/revaliases
 
 echo "Setup daemon.conf"
 echo "DbHost = "$DB_HOST
@@ -7,11 +10,11 @@ echo "DbName = "$DB_NAME
 echo "DbUser = "$DB_USER
 echo "DbPass = "$DB_PASS
 
-echo "DbHost = "$DB_HOST > /etc/p4d/daemon.conf
-echo "DbPort = "$DB_PORT >> /etc/p4d/daemon.conf
-echo "DbName = "$DB_NAME >> /etc/p4d/daemon.conf
-echo "DbUser = "$DB_USER >> /etc/p4d/daemon.conf
-echo "DbPass = "$DB_PASS >> /etc/p4d/daemon.conf
+echo "DbHost = "$DB_HOST > /home/p4d/daemon.conf
+echo "DbPort = "$DB_PORT >> /home/p4d/daemon.conf
+echo "DbName = "$DB_NAME >> /home/p4d/daemon.conf
+echo "DbUser = "$DB_USER >> /home/p4d/daemon.conf
+echo "DbPass = "$DB_PASS >> /home/p4d/daemon.conf
 
 echo "Setup ssmtp.conf"
 echo "root="$SMTP_MAIL_FROM
@@ -23,19 +26,17 @@ echo "UseSTARTTLS="$SMTP_STARTTLS
 echo "AuthUser="$SMTP_USERNAME
 echo "AuthPass="$SMTP_PASSWORD
 
-echo "root="$SMTP_MAIL_FROM > /etc/ssmtp/ssmtp.conf
-echo "mailhub="$SMTP_SERVER >> /etc/ssmtp/ssmtp.conf
-echo "hostname="$SMTP_NAME >> /etc/ssmtp/ssmtp.conf
-echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
-echo "UseTLS="$SMTP_TLS >> /etc/ssmtp/ssmtp.conf
-echo "UseSTARTTLS="$SMTP_STARTTLS >> /etc/ssmtp/ssmtp.conf
-echo "AuthUser="$SMTP_USERNAME >> /etc/ssmtp/ssmtp.conf
-echo "AuthPass="$SMTP_PASSWORD >> /etc/ssmtp/ssmtp.conf
+echo "root="$SMTP_MAIL_FROM > /home/p4d/ssmtp.conf
+echo "mailhub="$SMTP_SERVER >> /home/p4d/ssmtp.conf
+echo "hostname="$SMTP_NAME >> /home/p4d/ssmtp.conf
+echo "FromLineOverride=YES" >> /home/p4d/ssmtp.conf
+echo "UseTLS="$SMTP_TLS >> /home/p4d/ssmtp.conf
+echo "UseSTARTTLS="$SMTP_STARTTLS >> /home/p4d/ssmtp.conf
+echo "AuthUser="$SMTP_USERNAME >> /home/p4d/ssmtp.conf
+echo "AuthPass="$SMTP_PASSWORD >> /home/p4d/ssmtp.conf
 
 echo "root:"$SMTP_MAIL_FROM:$SMTP_SERVER
 
-echo "root:"$SMTP_MAIL_FROM:$SMTP_SERVER >> /etc/ssmtp/revaliases
+echo "root:"$SMTP_MAIL_FROM:$SMTP_SERVER >> /home/p4d/revaliases
 
-echo "Start p4d"
-/usr/bin/p4d -n -t
-
+/home/p4d/p4d.sh
